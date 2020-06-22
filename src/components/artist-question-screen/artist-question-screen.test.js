bd7d5b67dbcf62c246e16d07e5243d79a1838697
kny45 <kny.komarov@gmail.com>
2020-06-22 18:16:17 +0300

    Добавит snapshot-тест для ArtistQuestionScreen

---
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ArtistQuestionScreen from './artist-question-screen.jsx';
+
+describe(`Render ArtistQuestionScreen`, () => {
+  it(`ArtistQuestionScreen is rendered correctly`, () => {
+    const question = {
+      type: `artist`,
+      song: {
+        artist: `Jim Beam`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      },
+      answers: [{
+        picture: `https://api.adorable.io/avatars/128/0`,
+        artist: `William Lawson`,
+      }, {
+        picture: `https://api.adorable.io/avatars/128/1`,
+        artist: `Jack Daniels`,
+      }, {
+        picture: `https://api.adorable.io/avatars/128/2`,
+        artist: `Jim Beam`,
+      }],
+    };
+
+    const tree = renderer.create(
+        <ArtistQuestionScreen
+          question={question}
+          onAnswer={() => {}}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
