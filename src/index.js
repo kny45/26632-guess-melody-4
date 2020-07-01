bbbf825fceffe5780e8808fb0b21b9c23e3f3337
kny45 <kny.komarov@gmail.com>
2020-07-01 23:15:07 +0300

    Установит redux, react-redux, создаст начальную структуру (reducer, action, store) и подключит ReduxDevTools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
 import App from "./components/app/app.jsx";
+import {reducer} from "./reducer.js";
 
 import questions from "./mocks/questions.js";
 
@@ -9,10 +12,17 @@ const Settings = {
   ERROR_COUNT: 3
 };
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      errorCount = {Settings.ERROR_COUNT}
-      questions = {questions}
-    />,
+    <Provider store = {store}>
+      <App
+        errorCount = {Settings.ERROR_COUNT}
+        questions = {questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
